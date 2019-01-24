512ef02b0fac5851d9c7c38d7cc5c26d775a7c9c
Aditya Putra Irawan <adtputr@gmail.com>
2019-01-24 08:57:25 +0700

    baru

--- app/src/main/java/com/sikawan/sikawan/LaporLanjutanActivity.kt
+++ app/src/main/java/com/sikawan/sikawan/LaporLanjutanActivity.kt
@@ -4,15 +4,24 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import kotlinx.android.synthetic.main.activity_lapor_lanjutan.*
 
 class LaporLanjutanActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lapor_lanjutan)
+
+        btnLaporkanKeluhanLanjut.setOnClickListener {
+            val intent = Intent(this@LaporLanjutanActivity, LaporFeedback::class.java)
+            startActivity(intent)
+        }
+
     }
+
     fun back(view: View) {
         finish()
     }
+
 
 }
